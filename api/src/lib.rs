beec1b025867abf96935c9480d660b1c880cf505
ksh <ksh@bestco.kr>
2024-10-23 18:11:38 +0900

    ss

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -1,10 +1,11 @@
 use core::str;
 use std::io::prelude::*;
 use std::net::TcpStream;
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 use tokio::runtime::Runtime;
-
+use tracing::{info,warn};
 const INIT:[u8; 11]= [0xAA, 0xAA, 0xFF, 0x08, 0xC1, 0x02, 0x05, 0x00, 0x00, 0xCF, 0x73];
 
 struct Payload{
@@ -25,20 +26,47 @@ impl Payload {
 }
 
 pub fn test_thread(
-
+    str_mem:Arc<Mutex<Vec<String>>>
 ){
     thread::spawn(move||{
         let rt = Runtime::new().unwrap();
         rt.block_on(async {
             let mut stream = TcpStream::connect("192.168.0.200:200").unwrap();
+            info!("OPEN SOCKET IP : 192.168.0.200:200");
             stream.write(&INIT).unwrap();
             thread::sleep(Duration::from_secs(1));
             stream.write(&INIT).unwrap();
+            let mut hex_init = String::new();
+            INIT.map(|x|
+                {
+                    hex_init.push_str(format!("{:#X},",x).as_str());
+                }
+            );
+            info!("SEND SIGNAL : {:?}",hex_init);
             let mut buffer = vec![0; 128];
             loop{
-                if let Ok(data)=stream.read(&mut buffer){
-                    
+                let mut buffer = vec![0; 128];
+                if let Ok(data) = stream.read(&mut buffer){
+                    if buffer[0]==0xAA&&buffer[1]==0xAA&&buffer[2]==0xFF&&buffer[3]==0x18{
+                        // let asd =format!("{:#X}",buffer[2]);
+                        let mut hex_code = String::new();
+                        for (i,data) in buffer.iter().enumerate(){
+                            match i {
+                                0..25=>hex_code.push_str(format!("{:#X},",*data).as_str()),
+                                26=>hex_code.push_str(format!("{:#X}",*data).as_str()),
+                                _=>{
+                                }
+                            }
+                        }
+                        // buffer.iter().enumerate().map(|(i,data)|{
+                        // });
+                        (*str_mem.lock().unwrap()).push(hex_code);
+                        println!("{:?}",buffer);
+                    }
                 }
+                // (*str_mem.lock().unwrap()).push(format!("{:#X}",buffer[2]));
+                // println!("{:?}",buffer);
+                // thread::sleep(Duration::from_millis(1));
             }
         })
     });
--- runner/src/main.rs
+++ runner/src/main.rs
@@ -1,8 +1,24 @@
-use eframe::egui::{self, Vec2, ViewportBuilder};
-use std::net::TcpStream;
+use eframe::egui::{self, menu, Color32, RichText, Vec2, ViewportBuilder};
+use egui_tracing::EventCollector;
+use tracing::Level;
+use tracing::{info,warn};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use std::{net::TcpStream, sync::{Arc, Mutex}};
+use api::test_thread;
 
 
+#[derive(Default)]
+enum App_menu{
+    #[default]
+    RAWDATA,
+    LOGVIEW,
+}
 fn main() {
+    let collector = egui_tracing::EventCollector::default().with_level(Level::INFO);
+
+    tracing_subscriber::registry()
+        .with(collector.clone())
+        .init();
 
     let windows = ViewportBuilder{
         title: Some(String::from("RND APP")),
@@ -20,21 +36,34 @@ fn main() {
         ..Default::default()
     };
     let native_options = eframe::NativeOptions::default();
-    eframe::run_native("My egui App", options , Box::new(|cc| Ok(Box::new(MyEguiApp::new(cc)))));
+    eframe::run_native("My egui App", options , Box::new(|cc| 
+        {   
+            let mut app = MyEguiApp::new(cc,collector);
+            test_thread(app.str_mem.clone());
+            Ok(Box::new(app))
+        }
+    ));
 }
 
 #[derive(Default)]
 struct MyEguiApp {
-    
+    app_menu:App_menu,
+    collector:EventCollector,
+    str_mem:Arc<Mutex<Vec<String>>>,
 }
 
 impl MyEguiApp {
-    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+    fn new(cc: &eframe::CreationContext<'_>,collector:EventCollector) -> Self {
         // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
         // Restore app state using cc.storage (requires the "persistence" feature).
         // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
         // for e.g. egui::PaintCallback.
-        Self::default()
+        // Self::default()
+        Self{
+            collector,
+            app_menu:App_menu::RAWDATA,
+            str_mem:Arc::new(Mutex::new(vec![]))
+        }
     }
 }
 
@@ -42,12 +71,59 @@ impl eframe::App for MyEguiApp {
    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
     ctx.request_repaint();
        egui::CentralPanel::default().show(ctx, |ui| {
-        ui.heading("Hello World!");
-            egui::ScrollArea::vertical().show(ui, |ui| {
-                // Add a lot of widgets here.
-
-            });
-           
+        menu::bar(ui, |ui| {
+            ui.menu_button(
+                RichText::new(
+                    "🖥Main"
+                    // format!("{} Main", egui_nerdfonts::regular::HOME)
+                )
+                    .strong()
+                    .size(22.0)
+                    .color(Color32::from_rgb(38, 150, 255)), 
+                |ui|{
+                    if ui.button("RAW DATA VIEW").clicked() {
+                        self.app_menu = App_menu::RAWDATA;
+                    }
+                    if ui.button("LOG View").clicked() {
+                        self.app_menu = App_menu::LOGVIEW;
+                    }
+                }
+            );
+            ui.menu_button(
+                RichText::new(
+                    "TEMP"
+                    // format!("{} Main", egui_nerdfonts::regular::HOME)
+                )
+                    .strong()
+                    .size(22.0)
+                    .color(Color32::from_rgb(38, 150, 255)), 
+                |ui|{
+                    if ui.button("Home").clicked() {
+                        // …
+                    }
+                }
+            );
+        });
+            match self.app_menu {
+                App_menu::RAWDATA=>{
+                    egui::ScrollArea::vertical().show(ui, |ui| {
+                        // Add a lot of widgets here.
+                        let list = (*self.str_mem.lock().unwrap()).clone();
+                        for i in list{
+                            ui.label(i.as_str());
+                        }
+                        // ui.label(self.str_mem.lock().unwrap().as_str());
+                    });
+                    egui::SidePanel::right("my_left_panel").show(ctx, |ui| {
+                        ui.button("text")
+                     });
+                },
+                App_menu::LOGVIEW=>{
+                    egui::ScrollArea::both().show(ui, |ui|{
+                        ui.add(egui_tracing::Logs::new(self.collector.clone()))
+                    });
+                },
+            }
        });
    }
 }
